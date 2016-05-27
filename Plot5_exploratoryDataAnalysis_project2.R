c46ca96b61b1050c8803bf9e50b8fcf5ea3f903a
Douglas Whitten <whitten3@msu.edu>
2016-05-27 15:20:14 -0400

    Update Plot5_exploratoryDataAnalysis_project2.R

--- Plot5_exploratoryDataAnalysis_project2.R
+++ Plot5_exploratoryDataAnalysis_project2.R
@@ -1,10 +1,14 @@
-subSCC<-subset(SCC, subset=Data.Category=="Onroad",
+png(file="~/coursera/plot2.png", width=480, height=480)
+
+autosSCC<-subset(SCC, subset=Data.Category=="Onroad",
                select=c("SCC", "Data.Category"))
 
-newNEI<-merge(NEI, subSCC, by="SCC")
+autosNEI<-merge(NEI, subSCC, by="SCC")
 
-NEI3<-subset(newNEI, fips=="24510")
+balautoNEI<-subset(autosNEI, fips=="24510")
 
-plot(aggregate(Emissions~year, NEI3, sum), main=
+plot(aggregate(Emissions~year, balautoNEI, sum), main=
        "Motor Vehicle Emissions, Baltimore City, MA",
      xlab="Year", ylab="PM2.5 Emissions (tons)")
+
+dev.off()
