c542fc53b8da5f492d7c961756990a1fa6e40f8e
Douglas Whitten <whitten3@msu.edu>
2016-05-27 15:18:35 -0400

    Update Plot1_exploratoryDataAnalysis_project2.R

--- Plot1_exploratoryDataAnalysis_project2.R
+++ Plot1_exploratoryDataAnalysis_project2.R
@@ -1,3 +1,8 @@
+
+png(file="~/coursera/plot2.png", width=480, height=480)
+
 plot(aggregate(Emissions~year, NEI, sum), 
      main="Total PM2.5 Emissions in the US", xlab="Year", 
      ylab="Total Emissions (tons)")
+
+dev.off()
