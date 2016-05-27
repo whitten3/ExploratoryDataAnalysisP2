34bac931445016416773e4e51fea90d63cf7c781
Douglas Whitten <whitten3@msu.edu>
2016-05-27 15:19:54 -0400

    Update Plot4_exploratoryDataAnalysis_project2.R

--- Plot4_exploratoryDataAnalysis_project2.R
+++ Plot4_exploratoryDataAnalysis_project2.R
@@ -1,3 +1,5 @@
+png(file="~/coursera/plot2.png", width=480, height=480)
+
 coalSCC<-SCC[grep("Coal", SCC$EI.Sector),]
 
 coalNEI<-merge(NEI, coalSCC, by="SCC")
@@ -5,3 +7,5 @@ coalNEI<-merge(NEI, coalSCC, by="SCC")
 plot(aggregate(Emissions~year, coalNEI, sum), main=
          "Emissions From Coal Combustion in The US", 
        xlab="Year", ylab="PM2.5 Emissions (tons)")
+
+dev.off()
