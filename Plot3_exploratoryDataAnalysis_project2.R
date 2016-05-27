4e7d3ec6b0054ed7de47cacad62f8377350a12e1
Douglas Whitten <whitten3@msu.edu>
2016-05-27 15:19:31 -0400

    Update Plot3_exploratoryDataAnalysis_project2.R

--- Plot3_exploratoryDataAnalysis_project2.R
+++ Plot3_exploratoryDataAnalysis_project2.R
@@ -1,7 +1,17 @@
+
 library(ggplot2)
 
-NEI2<-subset(NEI, fips=="24510")
+png(file="~/coursera/plot2.png", width=480, height=480)
+
+baltimoreNEI<-subset(NEI, fips=="24510")
 
 
- plot<-qplot(year, Emissions, data=NEI2, facets=.~type, col=type)
+plot<-qplot(year, Emissions, data=baltimoreNEI, facets=
+                .~type, col=type, main="Emissions by Category, 
+              Baltimore City, MA", xlab="Year", ylab="Emissions
+              (tons)")
+ 
+ 
  plot+stat_smooth(method="lm")
+ 
+ dev.off()
